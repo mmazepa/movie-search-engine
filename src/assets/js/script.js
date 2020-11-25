6a1eeacc7ee3d3b839d38b26464415fc68da578b
mmazepa <maksymilianmariusz@gmail.com>
2020-11-25 16:44:35 +0100

    Add Less. External files for style and script.

---
+++ src/assets/js/script.js
@@ -0,0 +1,10 @@
+jQuery(function($) {
+    $(document).on('click', '.lightboxgallery-gallery-item', function(event) {
+        event.preventDefault();
+        $(this).lightboxgallery({
+            showCounter: true,
+            showTitle: true,
+            showDescription: true
+        });
+    });
+});
